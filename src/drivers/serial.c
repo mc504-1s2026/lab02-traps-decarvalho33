2cdfbbe514d798fba2e35188e6c909a50778aaff
decarvalho33 <alvarodecarvalho33@gmail.com>
2026-07-16 14:45:41 -0300

    lab02 finished

--- src/drivers/serial.c
+++ src/drivers/serial.c
@@ -1,44 +1,99 @@
 #include <kernel/serial.h>
 #include <kernel/panic.h>
+#include <arch/plic.h>
+#include <arch/spinlock.h>
+#include <arch/csr.h>
+
+#define SERIAL_BUF_SIZE 1024
+
+struct serial_dev {
+    char buf[SERIAL_BUF_SIZE];
+    size_t len;
+    struct spinlock lock;
+} dev = {0};
+
+static inline u8 serial_read_reg(u64 offset)
+{
+    return *(volatile u8 *)(SERIAL_BASE + offset);
+}
+
+static inline void serial_write_reg(u64 offset, u8 val)
+{
+    *(volatile u8 *)(SERIAL_BASE + offset) = val;
+}
 
 void serial_init()
 {
-	/* not implemented */
-	BUG();
+    /* desabilita  */
+    serial_write_reg(SERIAL_IER, 0x00);
+
+    /* 8 bitsword len */
+    serial_write_reg(SERIAL_LCR, 0x03);
+
+    /* FIFO pra RX TX e LIMPAA */
+    serial_write_reg(SERIAL_FCR, SERIAL_FCR_FIFO_ENABLE | SERIAL_FCR_RX_FIFO_CLEAR | SERIAL_FCR_TX_FIFO_CLEAR);
+    serial_write_reg(SERIAL_IER, SERIAL_IER_ERBFI);
 }
 
 void serial_irq_enable()
 {
-	/* not implemented */
-	BUG();
+    plic_irq_set_priority(IRQ_SERIAL, 1);
+    plic_hart_set_threshold(0, 0);
+    plic_hart_enable_irq(0, IRQ_SERIAL);
+
+    csr_set(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq_disable()
 {
-	/* not implemented */
-	BUG();
+
+    plic_irq_set_priority(IRQ_SERIAL, 0);
+    csr_clear(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq()
 {
-	/* not implemented */
-	BUG();
+    u32 irq = plic_hart_claim_irq(0);
+    
+    if (irq == IRQ_SERIAL) {
+        spin_lock(&dev.lock);
+        /*  buffer */
+        while (serial_read_reg(SERIAL_LSR) & SERIAL_LSR_DTR) {
+            char c = serial_read_reg(SERIAL_RBR);
+            if (dev.len < SERIAL_BUF_SIZE) {
+                dev.buf[dev.len++] = c;
+            }
+        }
+        spin_unlock(&dev.lock);
+    }
+    
+    if (irq) {
+        plic_hart_complete_irq(0, irq);
+    }
 }
 
 size_t serial_read(char *buf)
 {
-	/* not implemented */
-	BUG();
-}
+    u64 flags = spin_lock_irqsave(&dev.lock);
+    size_t size = dev.len;
+    for (size_t i = 0; i < size; i++) {
+        buf[i] = dev.buf[i];
+    }
+    dev.len = 0;
+    spin_unlock_irqrestore(&dev.lock, flags);
 
-void serial_puts(char *str)
-{
-	/* not implemented */
-	BUG();
+    return size;
 }
 
 void serial_putc(char c)
 {
-	/* not implemented */
-	BUG();
+    while ((serial_read_reg(SERIAL_LSR) & SERIAL_LSR_THRE) == 0) { }
+    serial_write_reg(SERIAL_THR, c);
+}
+
+void serial_puts(char *str)
+{
+    while (*str) {
+        serial_putc(*str++);
+    }
 }
--- src/kmain.c
+++ src/kmain.c
@@ -3,23 +3,77 @@
 #include <arch/timer.h>
 #include <kernel/trap.h>
 #include <kernel/serial.h>
+#include <kernel/string.h>
 
 extern int _hartid[];
+
+#define CMD_BUF_SIZE 256
+
 void kmain()
 {
-	printk_set_level(LOG_DEBUG);
-	info("entered S-mode\n");
-	info("booting on hart %d\n", _hartid[0]);
-	info("setting up virtual memory...\n");
-	vm_init();
+    printk_set_level(LOG_DEBUG);
+    info("entered S-mode\n");
+    info("booting on hart %d\n", _hartid[0]);
+    info("setting up virtual memory...\n");
+    vm_init();
 
-	info("enabling traps...\n");
-	trap_setup();
-	info("enabling timer...\n");
-	timer_irq_enable();
-	info("enabling serial...\n");
-	serial_init();
-	serial_irq_enable();
+    info("enabling traps...\n");
+    trap_setup();
+    info("enabling timer...\n");
+    timer_irq_enable();
+    info("enabling serial...\n");
+    serial_init();
+    serial_irq_enable();
+    hart_irq_enable();
 
-	/* implement your shell here */
+    char cmd_buf[CMD_BUF_SIZE];
+    size_t cmd_len = 0;
+    char rx_buf[64];
+
+    serial_puts("> ");
+
+    while (1) {
+        size_t n = serial_read(rx_buf);
+        
+        for (size_t i = 0; i < n; i++) {
+            char c = rx_buf[i];
+            
+            if (c == '\r' || c == '\n') {
+                serial_puts("\n");
+                cmd_buf[cmd_len] = '\0';
+                
+                if (cmd_len > 0) {
+                    if (strncmp(cmd_buf, "uptime", 6) == 0) {
+                        u64 t = timer_read() / TIMER_FREQ;
+                        printk(LOG_INFO, "%lus\n", t);
+                    } else if (strncmp(cmd_buf, "echo ", 5) == 0) {
+                        printk(LOG_INFO, "%s\n", cmd_buf + 5);
+                    } else if (strncmp(cmd_buf, "alarm ", 6) == 0) {
+                        u64 delay = strtou64(cmd_buf + 6, 10);
+                        timer_set_alarm(delay);
+                    } else if (strncmp(cmd_buf, "echo", 4) == 0) {
+                        printk(LOG_INFO, "\n");
+                    } else {
+                        printk(LOG_INFO, "command not found\n");
+                    }
+                }
+                
+                cmd_len = 0;
+                serial_puts("> ");
+                
+            } else if (c == '\b' || c == 0x7F) {
+                /* cosmetico */
+                if (cmd_len > 0) {
+                    cmd_len--;
+                    serial_puts("\b \b");
+                }
+            } else {
+                if (cmd_len < CMD_BUF_SIZE - 1) {
+                    cmd_buf[cmd_len++] = c;
+                    /* echo c */
+                    serial_putc(c); 
+                }
+            }
+        }
+    }
 }
